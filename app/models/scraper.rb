2b33d6df8fcb29dca5e3d0403f47b9a87d71d1e9
redfit <nagita.0724.0812@gmail.com>
2024-04-30 13:31:57 +0900

    Scraping Web Pages with Ruby

---
+++ app/models/scraper.rb
@@ -0,0 +1,16 @@
+class Scraper
+  attr_reader :document
+
+  def initialize(url)
+    response = Net::HTTP.get(URI(url))
+    @document = Nokogiri::Html(response)
+  end
+
+  def text(selector:)
+    document.at_css(selector).text
+  end
+
+  def present?(selector:)
+    document.at_css(selector).present?
+  end
+end
